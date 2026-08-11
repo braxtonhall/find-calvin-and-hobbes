eaae69b126c4c71736fec74a4bc3d1c3c88a435c
braxtonhall <braxtonjhall@gmail.com>
2026-08-12 01:13:36 +0200

    Fun the formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,15 @@
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default tseslint.config(
-	...tseslint.configs.recommended,
-	eslintConfigPrettier,
-	{
-		rules: {
-			"@typescript-eslint/no-unused-vars": [
-				"error",
-				{
-					argsIgnorePattern: "^_",
-					varsIgnorePattern: "^_",
-					destructuredArrayIgnorePattern: "^_",
-				},
-			],
-		},
+export default tseslint.config(...tseslint.configs.recommended, eslintConfigPrettier, {
+	rules: {
+		"@typescript-eslint/no-unused-vars": [
+			"error",
+			{
+				argsIgnorePattern: "^_",
+				varsIgnorePattern: "^_",
+				destructuredArrayIgnorePattern: "^_",
+			},
+		],
 	},
-);
+});
